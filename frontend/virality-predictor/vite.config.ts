3d523be4140ae6c755b918756f6a64f4914d74ac
TylerKunimitsu <tkk2601@iolani.org>
2026-04-08 11:00:35 -1000

    update vite config

--- frontend/virality-predictor/vite.config.ts
+++ frontend/virality-predictor/vite.config.ts
@@ -4,5 +4,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 8080,
+    hmr: {
+      host: "localhost", 
+    },
+  },
   plugins: [react()],
+  base: "/",
 })
